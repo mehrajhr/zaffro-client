425a4c5f46ee200c8d1db5b430dcd753d00d1e53
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2025-09-28 23:29:13 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-col justify-center items-center min-h-screen'>
+        <h1 className='text-4xl text-center'>Let’s start something new for Zaffro.</h1>
+        <p className='text-center text-sm mt-2'>Are you ready ?</p>
+      </div>
+    </>
+  )
+}
+
+export default App
